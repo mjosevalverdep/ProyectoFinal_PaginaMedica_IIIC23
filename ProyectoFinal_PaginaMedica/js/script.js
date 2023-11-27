2ccdc74a7320e3e60b1de579e55ac5058d88e777
Velsazar <jangulo10746@ufide.ac.cr>
2023-11-27 14:32:51 -0600

    add
    add funciones y validacion de correo y numero telefonico

--- ProyectoFinal_PaginaMedica/js/script.js
+++ ProyectoFinal_PaginaMedica/js/script.js
@@ -9,3 +9,17 @@ document.getElementById('appointmentForm').addEventListener('submit', function (
     event.preventDefault();
     document.querySelector('.alert').style.display = 'block';
 });
+
+
+//funcion para correo y numero de teleno en citas
+function isValidEmail(email) {
+    // Patrón básico de validación de correo electrónico
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function isValidPhone(phone) {
+    // Patrón básico de validación de número de teléfono
+    var phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(phone);
+}
