c5a9a6721fce6a8523b74fc02795da5468afa99e
Velsazar <jangulo10746@ufide.ac.cr>
2023-11-27 14:23:43 -0600

    Ajustes en paginas
    Ajustes en relacion a Citas

--- ProyectoFinal_PaginaMedica/js/script.js
+++ ProyectoFinal_PaginaMedica/js/script.js
@@ -3,3 +3,9 @@ function mostrarMensaje(enviar){
 alert("La informacion se ha enviado correctamente");
 
 }
+
+// script.js
+document.getElementById('appointmentForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    document.querySelector('.alert').style.display = 'block';
+});
