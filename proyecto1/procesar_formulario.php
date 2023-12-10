5841826270bc8e8f6086db57a0d3d5947f7c3dfa
Velsazar <jangulo10746@ufide.ac.cr>
2023-12-10 15:03:06 -0600

    contactenos
    se agrega funcinalidad a consultas y funcionalidad con la base de datos
    tambien se agrega la creacion de la tabla para la base da datos usando la DB usuarios

---
+++ proyecto1/procesar_formulario.php
@@ -0,0 +1,40 @@
+<?php
+$servidor = "localhost";
+$usuario = "Proyecto";
+$contrasena = "Proyecto";
+$base_de_datos = "usuarios";
+
+// Conectar a la base de datos
+$conn = new mysqli($servidor, $usuario, $contrasena, $base_de_datos);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recibir datos del formulario
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    // Insertar datos en la base de datos
+    $sql = "INSERT INTO Consultas (nombre, email, mensaje) VALUES ('$nombre', '$email', '$mensaje')";
+
+    if ($conn->query($sql) === TRUE) {
+        // Mensaje de éxito
+        $mensaje_exito = "Consulta enviada correctamente. Pronto nos pondremos en contacto contigo por correo electrónico.";
+        // Script de JavaScript para mostrar la alerta de confirmación
+        echo '<script>alert("' . $mensaje_exito . '"); window.location.href = "index.html";</script>';
+    } else {
+        $mensaje_error = "Error al enviar la consulta: " . $conn->error;
+        // Script de JavaScript para mostrar la alerta de error
+        echo '<script>alert("' . $mensaje_error . '"); window.location.href = "index.html";</script>';
+    }
+}
+
+// Cerrar la conexión a la base de datos
+$conn->close();
+?>
+
